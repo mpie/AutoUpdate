3811299112811ee98153e3e24181f2e5e645159f
Mpie <info@mpie.nl>
2014-03-25 23:39:40 +0100

    initial branch

--- README.md
+++
@@ -1,4 +0,0 @@
-AutoUpdate
-==========
-
-DooFree auto update
---
+++ plugin.video.doofree/default.py
@@ -0,0 +1,335 @@
+import re, urllib2, urllib, json, threading, os, zipfile, shutil
+import xbmcaddon, xbmcplugin, xbmcgui, xbmc
+
+ADDON = xbmcaddon.Addon(id='plugin.video.doofree')
+PATH = 'doofree'
+VERSION = '1.1.5'
+
+base_url = ''
+addon_handle = ''
+
+master_json = "https://raw.githubusercontent.com/mpie/doofree/REV-1.1.5/json/master.json?7"
+seesantv = "http://www.seesantv.com/seesantv_2014/"
+asia=["http://as11.seesantv.com/"]
+uk=["http://uk23.seesantv.com/", "http://uk24.seesantv.com/", "http://uk12.seesantv.com/", "http://uk13.seesantv.com/", "http://uk25.seesantv.com/", "http://uk1.seesantv.com/", "http://uk27.seesantv.com/"]
+us=["http://us14.seesantv.com/"]
+
+logo = xbmc.translatePath('special://home/addons/plugin.video.doofree/icon.png')
+datapath = xbmc.translatePath(ADDON.getAddonInfo('profile'))
+UpdatePath=os.path.join(datapath,'Update')
+try: os.makedirs(UpdatePath)
+except: pass
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+
+def unzipAndMove(_in, _out , src):
+    try:
+        zin = zipfile.ZipFile(_in, 'r')
+        zin.extractall(_out)
+        if src:
+            moveFiles(src, _out)
+            shutil.rmtree(src)
+    except Exception, e:
+        print str(e)
+        return False
+
+    return True
+
+def moveFiles(root_src_dir,root_dst_dir):
+    for src_dir, dirs, files in os.walk(root_src_dir):
+        dst_dir = src_dir.replace(root_src_dir, root_dst_dir)
+        if not os.path.exists(dst_dir):
+            os.mkdir(dst_dir)
+        for file_ in files:
+            src_file = os.path.join(src_dir, file_)
+            dst_file = os.path.join(dst_dir, file_)
+            if os.path.exists(dst_file):
+                os.remove(dst_file)
+            shutil.move(src_file, dst_dir)
+
+def getUpdateFile(path,default = 0):
+    if os.path.exists(path):
+        try:
+            f = open(path, 'r')
+            return f.read()
+        except: pass
+    return default
+
+def saveUpdateFile(path,value):
+    try:
+        open(path,'w+').write(value)
+    except: pass
+
+def CheckForAutoUpdate(force = False):
+    GitHubRepo    = 'doofree'
+    GitHubUser    = 'mpie'
+    GitHubBranch  = 'master'
+    GitHubPath    = 'tree/master/plugin.video.doofree'
+    UpdateVerFile = 'update'
+    RunningFile   = 'running'
+    verCheck=True #main.CheckVersion()#Checks If Plugin Version is up to date
+    if verCheck == True:
+        try:
+            print "DooFree auto update - started"
+            html=getContent('https://github.com/'+GitHubUser+'/'+GitHubRepo+'/'+GitHubPath+'?files=1')
+            print html
+        except: html=''
+        m = re.search("View (\d+) commit",html,re.I)
+        if m: gitver = int(m.group(1))
+        else: gitver = 0
+        UpdateVerPath = os.path.join(UpdatePath,UpdateVerFile)
+        try: locver = int(getUpdateFile(UpdateVerPath))
+        except: locver = 0
+        RunningFilePath = os.path.join(UpdatePath, RunningFile)
+        if locver < gitver and (not os.path.exists(RunningFilePath) or os.stat(RunningFilePath).st_mtime + 120 < time.time()) or force:
+            UpdateUrl = 'https://github.com/'+GitHubUser+'/'+GitHubRepo+'/archive/'+GitHubBranch+'.zip'
+            UpdateLocalName = GitHubRepo+'.zip'
+            UpdateDirName   = GitHubRepo+'-'+GitHubBranch
+            UpdateLocalFile = xbmc.translatePath(os.path.join(UpdatePath, UpdateLocalName))
+            main.setFile(RunningFilePath,'')
+            print "auto update - new update available ("+str(gitver)+")"
+            xbmc.executebuiltin("XBMC.Notification(DooFree Update,New Update detected,3000,"+logo+")")
+            xbmc.executebuiltin("XBMC.Notification(DooFree Update,Updating...,3000,"+logo+")")
+            try:os.remove(UpdateLocalFile)
+            except:pass
+            try: urllib.urlretrieve(UpdateUrl,UpdateLocalFile)
+            except:pass
+            if os.path.isfile(UpdateLocalFile):
+                extractFolder = xbmc.translatePath('special://home/addons')
+                pluginsrc =  xbmc.translatePath(os.path.join(extractFolder,UpdateDirName))
+                if unzipAndMove(UpdateLocalFile,extractFolder,pluginsrc):
+                    saveUpdateFile(UpdateVerPath,str(gitver))
+                    print "DooFree auto update - update install successful ("+str(gitver)+")"
+                    xbmc.executebuiltin("XBMC.Notification(DooFree Update,Successful,5000,"+logo+")")
+                    xbmc.executebuiltin("XBMC.Container.Refresh")
+                else:
+                    print "DooFree auto update - update install failed ("+str(gitver)+")"
+                    xbmc.executebuiltin("XBMC.Notification(DooFree Update,Failed,3000,"+logo+")")
+            else:
+                print "DooFree auto update - cannot find downloaded update ("+str(gitver)+")"
+                xbmc.executebuiltin("XBMC.Notification(DooFree Update,Failed,3000,"+logo+")")
+            try:os.remove(RunningFilePath)
+            except:pass
+        else:
+            if force: xbmc.executebuiltin("XBMC.Notification(DooFree Update,DooFree is up-to-date,3000,"+logo+")")
+            print "DooFree auto update - DooFree is up-to-date ("+str(locver)+")"
+        return
+
+def getContent(url):
+    content = urllib2.urlopen(url).read()
+    return content
+
+def parseJson(url):
+    req = urllib2.Request(url)
+    opener = urllib2.build_opener()
+    f = opener.open(req)
+    data = json.loads(f.read())
+    return data
+
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
+
+def addDirItem(url, name, image):
+    item = xbmcgui.ListItem(name, iconImage='DefaultFolder.png', thumbnailImage=image)
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=item, isFolder=True)
+
+def addDir(name, url, mode, image):
+    url = build_url({'mode': mode, 'name': name, 'url': url})
+    ok = addDirItem(url, name, image)
+    return ok
+
+def addThaiDir(name, url, mode, image):
+    url = build_url({'mode': mode, 'url': url})
+    ok = addDirItem(url, name, image)
+    return ok
+
+def addLink(name, url, mode, image):
+    url = build_url({'mode': mode, 'name': name, 'url': url, 'image': image})
+    item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=image)
+    item.setInfo(type="Video", infoLabels={ "Title": name })
+    ok = xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=item, isFolder=False)
+    return ok
+
+def addThaiLink(name, url, mode, image, channel):
+    url = build_url({'mode': mode, 'name': name.encode('tis-620'), 'url': url, 'channel': channel, 'image': image})
+    item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=image)
+    item.setInfo(type="Video", infoLabels={ "Title": name })
+    ok = xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=item, isFolder=False)
+    return ok
+
+def discoverPagination(link, url):
+    paginator=re.compile('<div class="page_list"  align="center">(.+?)</ul>').findall(link)[0]
+    pages=re.compile('>(\d+)</a>').findall(paginator)
+    if (len(pages) > 1):
+        for page in pages:
+            pag = int (page[0]) - 1
+            pageUrl = url + '&vdo_type=.mp4&page=' + str (pag)
+            addThaiDir('page ' + page[0], pageUrl, 2, image)
+
+def exists(url):
+    try:
+        r = urllib2.urlopen(url, timeout=1)
+        if r.getcode() == 200:
+            return True
+    except urllib2.URLError, e:
+        return False
+
+def HOME():
+    url = ''
+    data = parseJson(master_json)
+    for item in data['Home']:
+        if (exists(item['location'])):
+            url = item['location']
+        else:
+            if 'page' in item:
+                url = item['page']
+        addDir(item['title'].encode("utf-8"), url, 1, '')
+        url = ''
+    xbmcplugin.endOfDirectory(addon_handle)
+
+def INDEX(name, url):
+    if (exists(url)):
+        data = parseJson(url)
+        if (data['isFolder']):
+            for item in data['list']:
+                addDir(item['title'].encode("utf-8"), item['location'], 1, item['thumbnail'])
+        else:
+            for item in data['list']:
+                print item
+                addLink(item['title'].encode("utf-8"), item['location'], 4, item['thumbnail'])
+
+    else:
+        link = getContent(url)
+        link=''.join(link.splitlines()).replace('\'','"')
+        limatch=re.compile('<figure>(.+?)</a><figcaption>').findall(link)
+        for licontent in limatch:
+            show=re.compile('<a href="(.+?)"><img src="(.+?)" alt="(.+?)">').findall(licontent)
+            title = show[0][2].decode('tis-620')
+            addThaiDir(title, seesantv + show[0][0], 2, show[0][1])
+
+def getEpisodes(url):
+    link = getContent(url + '&vdo_type=.mp4')
+    link = ''.join(link.splitlines()).replace('\'','"')
+    link=''.join(link.splitlines()).replace('<i class="icon-new"></i>','')
+
+    episodematch = re.compile('<table class="program-archive">(.+?)</table>').findall(link)
+    episodes = re.compile('<a href="(.+?)" >(.+?)</a> </td>                           \t\t\t\t\t\t\t<td>\t\t\t\t\t\t\t\t<a href="(.+?)" ><img').findall(episodematch[0])
+
+    programMeta = re.compile('<div class="program-meta">(.+?)</div>').findall(link)
+    image = re.compile('<img src="(.+?)" alt').findall(programMeta[0])[0]
+
+    topInfo = re.compile('<div class="top-info">(.+?)</div>').findall(link)
+    channel = re.compile('<img src="(.+?)" width').findall(topInfo[0])
+    if (len(channel) > 0):
+        channel = 'ch' + channel[0][-5]
+        if (channel == 'chv'):
+            channel = 'chthaipbs'
+    else:
+        channel = 'chall'
+
+    for episode in episodes:
+        addThaiLink(episode[1].decode('tis-620'), seesantv + episode[0], 3, image, channel)
+    # paginator
+    discoverPagination(link, url)
+
+def getVideoUrl(name, url, channel):
+    trySD = True
+    programId = re.compile('program_id=(\d+)').findall(url)[0]
+    if (len(programId) < 5):
+        programId = '0' + programId
+    fullDate = re.compile('(\d+-\d+-\d+) ').findall(name)[0]
+    date = ''.join(fullDate.splitlines()).replace('-','')
+    hd = channel + '/' + programId + '/' + date + '1-' + programId + '.mp4'
+    sd = channel + '/' + programId + '/' + date + '-' + programId + '.mp4'
+    #for host in xrange(1, 31):
+    for host in uk:
+        #fullurl = "http://uk" + str (host) + ".seesantv.com/" + path
+        fullurl = host + hd
+        found = exists(fullurl)
+        if (found):
+            xbmc.Player().play(fullurl)
+            trySD = False
+            break
+    if (trySD):
+        for host in uk:
+            fullurl = host + sd
+            found = exists(fullurl)
+            if (found):
+                xbmc.Player().play(fullurl)
+                break
+
+def play(name, vidurl, image):
+    item = xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=image)
+    item.setInfo(type="Video", infoLabels={ "Title": name })
+    xbmc.Player().play(vidurl, item)
+
+def getParams():
+    param = []
+    paramstring = sys.argv[2]
+    if (len(paramstring)>= 2):
+        params = sys.argv[2]
+        cleanedparams = params.replace('?','')
+        if (params[len(params)-1] == '/'):
+            params = params[0:len(params)-2]
+        pairsofparams = cleanedparams.split('&')
+        param = {}
+        for i in range(len(pairsofparams)):
+            splitparams = {}
+            splitparams = pairsofparams[i].split('=')
+            if (len(splitparams) == 2):
+                param[splitparams[0]] = splitparams[1]
+    return param
+
+params=getParams()
+url=None
+name=None
+mode=None
+serverurl=None
+channel=None
+playpath=None
+image=None
+try:
+    url=urllib.unquote_plus(params['url'])
+except:
+    pass
+try:
+    name=urllib.unquote_plus(params['name'])
+except:
+    pass
+try:
+    mode=int(params['mode'])
+except:
+    pass
+try:
+    serverurl=urllib.unquote_plus(params['serverurl'])
+except:
+    pass
+try:
+    channel=urllib.unquote_plus(params['channel'])
+except:
+    pass
+try:
+    playpath=urllib.unquote_plus(params['playpath'])
+except:
+    pass
+try:
+    image=urllib.unquote_plus(params['image'])
+except:
+    pass
+
+sysarg=str(sys.argv[1])
+if mode==None or url==None or len(url)<1:
+    threading.Thread(target=CheckForAutoUpdate).start()
+    #INDEX("Test", "https://raw.githubusercontent.com/mpie/doofree/REV-1.1.5/json/new_movies.json?4")
+    HOME()
+elif mode==1:
+    INDEX(name, url)
+elif mode==2:
+    getEpisodes(url)
+elif mode==3:
+    getVideoUrl(name, url, channel)
+elif mode==4:
+    play(name, url, image)
+
+xbmcplugin.endOfDirectory(addon_handle)
