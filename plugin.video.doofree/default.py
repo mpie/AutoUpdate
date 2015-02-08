9dc808b4ee465e829aadea350682bf9df87af39f
Ai-yakaan <info@mpie.nl>
2015-02-09 00:11:22 +0100

    Fake commit

--- plugin.video.doofree/default.py
+++ plugin.video.doofree/default.py
@@ -239,7 +239,7 @@ class main:
         GitHubPath    = 'tree/master/plugin.video.doofree'
         UpdateVerFile = 'update'
         RunningFile   = 'running'
-        verCheck=True #main.CheckVersi
+        verCheck=True #main.CheckVers
         if verCheck == True:
             try:
                 print "DooFree auto update - started"
