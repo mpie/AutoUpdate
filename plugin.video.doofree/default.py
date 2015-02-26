80b7166870548657f8bd3ca0d1b6192f288ad954
Mpie <info@mpie.nl>
2015-02-26 20:18:02 +0100

    enabled cache

--- plugin.video.doofree/default.py
+++ plugin.video.doofree/default.py
@@ -871,8 +871,8 @@ class index:
             t1 = int(re.sub('[^0-9]', '', str(match[3])))
             t2 = int(datetime.datetime.now().strftime("%Y%m%d%H%M"))
             update = abs(t2 - t1) >= int(timeout*60)
-            #if update == False:
-                #return response
+            if update == False:
+                return response
         except:
             pass
 
